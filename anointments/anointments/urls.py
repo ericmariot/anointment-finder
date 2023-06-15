bbb722c10273652365fbaeaf4753427dd66d5d28
ericmariot <eric.mariot@betrybe.com>
2023-06-15 17:31:20 -0300

    fix: urls

--- anointments/anointments/urls.py
+++ anointments/anointments/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-    path("oils/", include("oils.urls")),
     path("admin/", admin.site.urls),
 ]
