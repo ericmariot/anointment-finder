cddec94f036be2650ea2a170fc2e17ef7a9a051c
ericmariot <eric.mariot@betrybe.com>
2023-06-15 18:03:00 -0300

    feat: build base database

--- anointments-finder/anointments-finder/settings.py
+++ anointments-finder/anointments-finder/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "anointments.apps.AnointmentsConfig",
     "oils.apps.OilsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
