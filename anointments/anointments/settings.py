8aed18c203db4cc71bc3bb8ec8f4ddef02c3288b
ericmariot <eric.mariot@betrybe.com>
2023-06-15 17:47:39 -0300

    fix: add oils to url and settings

--- anointments/anointments/settings.py
+++ anointments/anointments/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "oils.apps.OilsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
