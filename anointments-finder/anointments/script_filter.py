19087770fa3cbf8208a335fb68fc30b9b98af3e4
ericmariot <eric.mariot@betrybe.com>
2023-07-11 21:53:14 -0300

    fix: changes from code review

--- anointments-finder/anointments/script_filter.py
+++ anointments-finder/anointments/script_filter.py
@@ -2,20 +2,16 @@ from anointments.models import Anointment
 
 
 def find_anointments(oil_names: set[str]):
-    anointments = Anointment.objects.all()
+    # SELECT * FROM anointments_anointment
+    # SELECT * FROM anointments_anointment JOIN oils_oil ON anointments_anointment.oil_1_id = oils_oil.id JOIN oils_oil...;
+    anointments = Anointment.objects.select_related("oil_1", "oil_2", "oil_3").all()
+    condition = "intersection" if len(oil_names) <= 2 else "issubset"
     result = []
 
-    if len(oil_names) <= 2:
-        for anointment in anointments:
-            oil_set = {oil.name for oil in anointment.oils}
+    for anointment in anointments:
+        oil_set = {oil.name for oil in anointment.oils}
 
-            if oil_names.intersection(oil_set):
-                result.append(anointment)
-    else:
-        for anointment in anointments:
-            oil_set = {oil.name for oil in anointment.oils}
-
-            if oil_set.issubset(oil_names):
-                result.append(anointment)
+        if getattr(oil_set, condition)(oil_names):
+            result.append(anointment)
 
     return result
--- anointments-finder/anointments/tests/tests.py
+++ anointments-finder/anointments/tests/tests.py
@@ -1,23 +1,53 @@
 import pytest
 
 from anointments.script_filter import find_anointments
+from anointments.tests.factories import AnointmentFactory
 
 
 @pytest.mark.django_db
 class TestFindAnointments:
-    def test_with_one_oil_find_all_that_use_golden(self, golden_anointments):
+    def test_with_one_oil_find_all_that_use_golden(self):
+        golden = AnointmentFactory(oil_1__name="Golden Oil")
+        AnointmentFactory(
+            oil_1__name="Silver Oil", oil_2__name="Silver Oil", oil_3__name="Silver Oil"
+        )
+
         find_all = find_anointments({"Golden Oil"})
 
-        assert find_all == golden_anointments
+        assert find_all == [golden]
 
-    def test_with_one_oil_find_all_that_use_golden(
-        self, silver_or_opalescent_anointments
-    ):
+    def test_with_one_oil_find_all_that_use_silver_or_opalescent(self):
+        silver = AnointmentFactory(oil_1__name="Silver Oil")
+        opalescent = AnointmentFactory(oil_1__name="Opalescent Oil")
+        AnointmentFactory(
+            oil_1__name="Crimson Oil",
+            oil_2__name="Crimson Oil",
+            oil_3__name="Crimson Oil",
+        )
         find_all = find_anointments({"Silver Oil", "Opalescent Oil"})
 
-        assert find_all == silver_or_opalescent_anointments
+        assert silver in find_all
+        assert opalescent in find_all
+        assert len(find_all) == 2
 
-    def test_oil_combinations(self, many_anointments):
+    def test_oil_combinations(self):
+        anointments = {
+            AnointmentFactory(
+                oil_1__name="Black Oil",
+                oil_2__name="Crimson Oil",
+                oil_3__name="Violet Oil",
+            ),
+            AnointmentFactory(
+                oil_1__name="Teal Oil",
+                oil_2__name="Azure Oil",
+                oil_3__name="Indigo Oil",
+            ),
+        }
+        AnointmentFactory(
+            oil_1__name="Opalescent Oil",
+            oil_2__name="Silver Oil",
+            oil_3__name="Opalescent Oil",
+        )
         find_all = find_anointments(
             {
                 "Black Oil",
@@ -29,4 +59,4 @@ class TestFindAnointments:
             }
         )
 
-        assert find_all == many_anointments
+        assert set(find_all) == anointments
